1e92afc07dd152cc6c8f2b0bf8884f0af9b48829
cariasdaniel <d.carias@ymail.com>
2021-03-30 15:28:37 -0300

    -

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
